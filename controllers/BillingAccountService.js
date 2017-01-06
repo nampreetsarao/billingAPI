d1eeac22c1508a2c814ed2b97fc2101019accba6
nampreetsarao <nampreetsarao@gmail.com>
2017-01-06 12:21:38 +0530

    changes for bluemix5 compose

--- controllers/BillingAccountService.js
+++ controllers/BillingAccountService.js
@@ -2,40 +2,99 @@
 
 //Mongo connection
 
-var mongo = require('mongodb');
+// Then we'll pull in the database client library
+var MongoClient = require("mongodb").MongoClient;
 
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
+// Now lets get cfenv and ask it to parse the environment variable
+var cfenv = require('cfenv');
+var appenv = cfenv.getAppEnv();
 
-var server = new Server('sl-us-dal-9-portal.2.dblayer.com', 18254, {auto_reconnect: true});
-var db = new Db('billingAPIDB', server);
-//var promise = new Promise();
+// Within the application environment (appenv) there's a services object
+var services = appenv.services;
 
-db.open(function(err, db) {
-    if(!err) {
-      db.authenticate("admin", "OMEABBXDEUVWWZVF", function(err, res) {
-           if(!err) {
-               console.log("Authenticated");
-               db.collection('billingAccount', {strict:true}, function(err, collection) {
-                 // promise.resolve(db);
-                   if (err) {
-                       console.log("The 'billingAccount' collection doesn't exist. Creating it with sample data..."+err);
+// The services object is a map named by service so we extract the one for MongoDB
+var mongodb_services = services["compose-for-mongodb"];
 
-                       //populateDB();
-                   }else{
-                     console.log("Connected to 'billingAPIDB' database");
-                   }
-               });
-           } else {
-               console.log("Error in authentication.");
-               console.log(err);
-           }
-       });
-    }else{
-               console.log("Unable to connect:"+err);
+// This check ensures there is a services for MongoDB databases
+assert(!util.isUndefined(mongodb_services), "Must be bound to compose-for-mongodb services");
+
+// We now take the first bound MongoDB service and extract it's credentials object
+var credentials = mongodb_services[0].credentials;
+
+// Within the credentials, an entry ca_certificate_base64 contains the SSL pinning key
+// We convert that from a string into a Buffer entry in an array which we use when
+// connecting.
+var ca = [new Buffer(credentials.ca_certificate_base64, 'base64')];
+
+// This is a global variable we'll use for handing the MongoDB client around
+var mongodb;
+
+// This is the MongoDB connection. From the application environment, we got the
+// credentials and the credentials contain a URI for the database. Here, we
+// connect to that URI, and also pass a number of SSL settings to the
+// call. Among those SSL settings is the SSL CA, into which we pass the array
+// wrapped and now decoded ca_certificate_base64,
+MongoClient.connect(credentials.uri, {
+        mongos: {
+            ssl: true,
+            sslValidate: true,
+            sslCA: ca,
+            poolSize: 1,
+            reconnectTries: 1
+        }
+    },
+    function(err, db) {
+        // Here we handle the async response. This is a simple example and
+        // we're not going to inject the database connection into the
+        // middleware, just save it in a global variable, as long as there
+        // isn't an error.
+        if (err) {
+            console.log("Error occured while connecting: "+err);
+        } else {
+            // Although we have a connection, it's to the "admin" database
+            // of MongoDB deployment. In this example, we want the
+            // "examples" database so what we do here is create that
+            // connection using the current connection.
+            mongodb = db.db("billingAPIDB");
+            console.log("Connected sucessfuly to billingAPIDB!!");
+        }
     }
-});
+);
+
+// var mongo = require('mongodb');
+//
+// var Server = mongo.Server,
+//     Db = mongo.Db,
+//     BSON = mongo.BSONPure;
+//
+// var server = new Server('sl-us-dal-9-portal.2.dblayer.com', 18254, {auto_reconnect: true});
+// var db = new Db('billingAPIDB', server);
+// //var promise = new Promise();
+//
+// db.open(function(err, db) {
+//     if(!err) {
+//       db.authenticate("admin", "OMEABBXDEUVWWZVF", function(err, res) {
+//            if(!err) {
+//                console.log("Authenticated");
+//                db.collection('billingAccount', {strict:true}, function(err, collection) {
+//                  // promise.resolve(db);
+//                    if (err) {
+//                        console.log("The 'billingAccount' collection doesn't exist. Creating it with sample data..."+err);
+//
+//                        //populateDB();
+//                    }else{
+//                      console.log("Connected to 'billingAPIDB' database");
+//                    }
+//                });
+//            } else {
+//                console.log("Error in authentication.");
+//                console.log(err);
+//            }
+//        });
+//     }else{
+//                console.log("Unable to connect:"+err);
+//     }
+// });
 
 //Mongo connection end
 
